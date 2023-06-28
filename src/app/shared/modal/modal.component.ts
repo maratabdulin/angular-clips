75772d0813e47a452e1f4de91b05257b2813fe65
maratabdulin <abdulinmm@gmail.com>
2023-06-28 13:05:08 +0200

    add modal window

---
+++ src/app/shared/modal/modal.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import {ModalService} from "../../services/modal.service";
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.css']
+})
+export class ModalComponent {
+  constructor(public modal: ModalService) {
+  }
+
+  closeModal() {
+    this.modal.toggleModal()
+  }
+}
