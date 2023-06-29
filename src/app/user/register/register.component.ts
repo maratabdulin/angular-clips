63d3ef8a38a3c9db670171d1178206d2a334b9a5
maratabdulin <abdulinmm@gmail.com>
2023-06-29 13:06:24 +0200

    add validation on some field

---
+++ src/app/user/register/register.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import {FormControl, FormGroup, Validators } from "@angular/forms";
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent {
+  registerForm = new FormGroup({
+    name: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3)
+    ]),
+    email: new FormControl('', [
+      Validators.required,
+      Validators.email
+    ]),
+    age: new FormControl('',[
+      Validators.required,
+      Validators.min(18),
+      Validators.max(120)
+    ]),
+    password: new FormControl(''),
+    confirm_password: new FormControl(''),
+    phoneNumber: new FormControl(''),
+  })
+}
