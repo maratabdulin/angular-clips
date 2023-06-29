f62d4e4183cf1d6be26ba0773881d65e915dfdc6
maratabdulin <abdulinmm@gmail.com>
2023-06-29 11:02:48 +0200

    add tabs to modal window

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -13,7 +13,7 @@ export class NavComponent {
 
   openModal($event: Event) {
     $event.preventDefault()
-    this.modal.toggleModal()
+    this.modal.toggleModal('auth')
   }
 
 }
--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input, ElementRef, OnInit} from '@angular/core';
 import {ModalService} from "../../services/modal.service";
 
 @Component({
@@ -6,11 +6,16 @@ import {ModalService} from "../../services/modal.service";
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.css']
 })
-export class ModalComponent {
-  constructor(public modal: ModalService) {
+export class ModalComponent implements OnInit {
+  @Input() modalID = ''
+  constructor(public modal: ModalService, public el: ElementRef) {
   }
 
   closeModal() {
-    this.modal.toggleModal()
+    this.modal.toggleModal(this.modalID)
+  }
+
+  ngOnInit() {
+    document.body.appendChild(this.el.nativeElement)
   }
 }
