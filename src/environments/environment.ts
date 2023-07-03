1bddd31ffd0110a72254a213e8d94696e22d5a89
maratabdulin <abdulinmm@gmail.com>
2023-07-03 12:10:10 +0200

    add auth with supabase

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC-5ASf7i-nr2bDRqFwZcBJ_rsciTmX8Zk",
+    authDomain: "clips-ab20e.firebaseapp.com",
+    projectId: "clips-ab20e",
+    storageBucket: "clips-ab20e.appspot.com",
+    appId: "1:216480663465:web:3e68322f5f65319b73a6b8"
+  }
+};
