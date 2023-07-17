208fad710d2e8835ccbe189f0b7cad4d2f7e95c6
maratabdulin <abdulinmm@gmail.com>
2023-07-17 10:11:29 +0200

    add lazy loading

--- src/app/services/clip.service.ts
+++ src/app/services/clip.service.ts
@@ -9,12 +9,13 @@ import IClip from "../models/clip.model";
 import {AngularFireAuth} from "@angular/fire/compat/auth";
 import {BehaviorSubject, combineLatest, map, of, switchMap} from "rxjs";
 import {AngularFireStorage} from "@angular/fire/compat/storage";
+import {ActivatedRouteSnapshot, Resolve, Router, RouterStateSnapshot} from "@angular/router";
 
 
 @Injectable({
   providedIn: 'root'
 })
-export class ClipService {
+export class ClipService implements Resolve<IClip | null> {
   public clipsCollection: AngularFirestoreCollection<IClip>
   pageClips: IClip[] = []
   pendingRequest = false
@@ -22,7 +23,8 @@ export class ClipService {
   constructor(
     private db: AngularFirestore,
     private auth: AngularFireAuth,
-    private storage: AngularFireStorage
+    private storage: AngularFireStorage,
+    private router: Router
   ) {
     this.clipsCollection = db.collection('clips')
   }
@@ -104,5 +106,23 @@ export class ClipService {
     })
 
     this.pendingRequest = false
+  }
+
+  resolve(route:ActivatedRouteSnapshot, state:RouterStateSnapshot
+  ) {
+   return this.clipsCollection.doc(route.params['id'])
+     .get()
+     .pipe(
+       map(snapshot => {
+         const data = snapshot.data()
+
+         if(!data) {
+           this.router.navigate(['/'])
+           return null
+         }
+
+         return data
+       })
+     )
   }
 }
